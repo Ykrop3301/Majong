3e331970863a22a8f7efa4b909a90df6a5950e6a
Ykrop3301 <ykrop3301@gmail.com>
2025-12-09 15:49:01 +1100

    modified: win popup
     modified win popup when location ended

--- Assets/MajongGame/Scripts/Common/PopupSystem/PopupVariants/WinPopup.cs
+++ Assets/MajongGame/Scripts/Common/PopupSystem/PopupVariants/WinPopup.cs
@@ -1,10 +1,15 @@
 ﻿using MajongGame.Common.LevelSystem;
+using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 namespace MajongGame.Common.PopupSystem.PopupVariants
 {
     public class WinPopup : Popup
     {
+        [SerializeField] private Button _centerMainMenuButton;
+        [SerializeField] private Transform _twoButtonsHolder;
+
         private ILevelsController _levelsController;
         private SceneChanger _sceneChanger;
 
@@ -15,6 +20,13 @@ namespace MajongGame.Common.PopupSystem.PopupVariants
             _sceneChanger = sceneChanger;
         }
 
+        public override void Show()
+        {
+            base.Show();
+            if (_levelsController.CurrentLevel.location.LevelsCount - 1 == _levelsController.CurrentLevel.levelId)
+                OnLocationEnded();
+        }
+
         public void NextLevel()
         {
             Hide(() => _levelsController.PlayNextLevel());
@@ -23,6 +35,12 @@ namespace MajongGame.Common.PopupSystem.PopupVariants
         public void GoToMainMenu()
         {
             Hide(() => _sceneChanger.LoadScene("MenuScene"));
+        }
+
+        private void OnLocationEnded()
+        {
+            _centerMainMenuButton.gameObject.SetActive(true);
+            _twoButtonsHolder.gameObject.SetActive(false);
         }
     }
 }
